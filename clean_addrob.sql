b3f8daf2c907c1eb8a7645046d1f03e7cc0baca9
dennn66 <dennn6696@gmail.com>
2020-08-22 19:17:24 +0500

    first commit

--- clean_addrob.sql
+++ clean_addrob.sql
@@ -1,5 +1,3 @@
-DROP TABLE IF EXISTS TMP_AO; 
-CREATE TABLE TMP_AO  AS SELECT * FROM public.AO2 WHERE 0 = 1;
 INSERT INTO public.TMP_AO(
 	guid, parentguid, regioncode, postalcode, name, kod_t_st, housenum, eststatus, buildnum, strucnum, strstatus, cadnum, code, livestatus, statstatus,  operstatus, divtype, startdate, updatedate, enddate) SELECT 
     guid, parentguid, regioncode, postalcode, name, kod_t_st,     null,      null,     null,     null,      null, cadnum, code, livestatus,       null,  operstatus, divtype, startdate, updatedate, enddate FROM  public.TMP_ADDROB WHERE actstatus = 1;
--- clean_stead.sql
+++ clean_stead.sql
@@ -1,4 +1,2 @@
-DROP TABLE IF EXISTS TMP_AO; 
-CREATE TABLE TMP_AO  AS SELECT * FROM public.AO2 WHERE 0 = 1;
 INSERT INTO public.ao(
 	guid, parentguid, regioncode, postalcode, name, kod_t_st, housenum, eststatus, buildnum, strucnum, strstatus, cadnum, code, livestatus, statstatus, currstatus, operstatus, divtype, startdate, updatedate, enddate) SELECT guid, parentguid, regioncode, postalcode, name, kod_t_st, null, null, null, null, null, cadnum, null, livestatus, statstatus, null, operstatus, divtype, startdate, updatedate, enddate FROM  public.stead WHERE actstatus = 1;
