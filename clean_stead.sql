590704018bc010de2c39bf42ba2ec4762d65614e
Denis Evladov <denis.evladov@domru.ru>
2020-08-23 15:42:35 +0500

    .

--- clean_stead.sql
+++ clean_stead.sql
@@ -1,3 +1,3 @@
 INSERT INTO public.TMP_AO(
 	guid, parentguid, regioncode, postalcode, name, kod_t_st, housenum, eststatus, buildnum, strucnum, strstatus, cadnum, code, livestatus, statstatus, operstatus, divtype, startdate, updatedate, enddate) SELECT 
-    guid, parentguid, regioncode, postalcode, name, kod_t_st,     null,      null,     null,     null,      null, cadnum, null, livestatus, statstatus, operstatus, divtype, startdate, updatedate, enddate FROM  public.stead WHERE  nextid is null;;
+    guid, parentguid, regioncode, postalcode, name, kod_t_st,     null,      null,     null,     null,      null, cadnum, null, livestatus, null, operstatus, divtype, startdate, updatedate, enddate FROM  public.stead WHERE  nextid is null;;
