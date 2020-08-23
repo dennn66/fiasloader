f8476418a9ede7870d602145da7c4a9fd98407a9
Denis Evladov <denis.evladov@domru.ru>
2020-08-23 18:57:03 +0500

    .

--- import_dict.sh
+++ import_dict.sh
@@ -6,7 +6,7 @@ for TBL_TO_LOAD in $TBLS_TO_LOAD
 do
   echo "++++++++++++++++++ LOADING TABLE = $TBL_TO_LOAD"
   pgdbf $PATH_TO_DBF_FILES/$TBL_TO_LOAD.DBF | iconv -f cp866 -t utf-8 | psql postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB
-  if [-f ./$(dirname $0)/init_${TBL_TO_LOAD,,}.sql]; then
+  if test -f ./$(dirname $0)/init_${TBL_TO_LOAD,,}.sql; then
     psql postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB -f ./$(dirname $0)/init_${TBL_TO_LOAD,,}.sql  
   fi
 done
