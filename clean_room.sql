d45b84bdadba251d697690c7c095ae1719632402
dennn66 <dennn6696@gmail.com>
2020-08-26 02:01:32 +0500

    .

--- clean_room.sql
+++ clean_room.sql
@@ -1,6 +1,6 @@
 INSERT INTO public.TMP_AO(
-	guid, parentguid, regioncode, postalcode, name, kod_t_st,   housenum, eststatus, buildnum,   strucnum, strstatus, cadnum, code, livestatus, statstatus, operstatus, divtype, startdate, updatedate, enddate) SELECT 
-    guid, parentguid, regioncode, postalcode, null,     null, flatnumber,  flattype,     null, roomnumber,  roomtype, cadnum, null, livestatus,       null, operstatus,       0, startdate, updatedate, enddate FROM  public.TMP_ROOM WHERE  nextid is null;
+	guid, parentguid, regioncode, postalcode, name, kod_t_st,   housenum,      eststatus, buildnum,   strucnum,      strstatus, cadnum, code, livestatus, statstatus, operstatus, divtype, startdate, updatedate, enddate) SELECT 
+    guid, parentguid, regioncode, postalcode, null,     null, flatnumber,  flattype::int,     null, roomnumber,  roomtype::int, cadnum, null, livestatus,       null, operstatus,       0, startdate, updatedate, enddate FROM  public.TMP_ROOM WHERE  nextid is null;
     
 --  flatnumber, flattype, roomnumber, roomtype,  roomcadnum
 UPDATE TMP_AO SET kod_t_st = flattype.kod_t_st FROM flattype WHERE flattype.fltypeid = TMP_AO.eststatus;
